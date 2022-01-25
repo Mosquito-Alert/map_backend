2a3ff52feda70a8d382087eecb4287de824a5144
Marc Compte <marc@braquets.cat>
2022-01-25 17:21:05 +0100

    Translate the site toolbar button labels.

--- project/translations.py
+++ project/translations.py
@@ -6,5 +6,11 @@ from django.utils import translation
 def translations(request, lang):
     translation.activate(lang)
     return JsonResponse({
-        "Open": _("Open")
+        "Open": _("Open"),
+        "Layers": _("Layers"),
+        "Models": _("Models"),
+        "Lang": _("Lang"),
+        "Share": _("Share"),
+        "Help": _("Help"),
+        "Log in": _("Log in")
     })
