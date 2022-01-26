b2b90a39e74e087522e731364baa6e3f22058635
Marc Compte <marc@braquets.cat>
2022-01-26 16:07:09 +0100

    Add rosetta for translation management.

--- project/urls.py
+++ project/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from .translations import translations
 
 urlpatterns = [
@@ -22,3 +23,8 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('translations/<lang>/', translations)
 ]
+
+if 'rosetta' in settings.INSTALLED_APPS:
+    urlpatterns += [
+        re_path(r'^rosetta/', include('rosetta.urls'))
+    ]
