ccaf4293c8e7ddd79b0eaf803e8c0db05f3b28ff
Marc Compte <marc@braquets.cat>
2022-01-28 13:51:47 +0100

    API. Get feature data.

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 """API urls."""
 from django.urls import path
+from .views import get_feature
 
 
 urlpatterns = [
+    path('get_feature/<id>/', get_feature)
     # path('admin/', admin.site.urls),
     # path('', include('api.urls'))
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def get_feature(request, id):
+    """Return a feature."""
+    return JsonResponse({
+        'layer': 'mosquito_tiger_confirmed',
+        'date': '21-02-2021',
+        'description': 'Blah, blah, blah',
+        # 'img': 'empty'
+    })
