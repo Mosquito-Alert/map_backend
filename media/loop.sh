46596a7e7c08a08921c34964b1e8d39e9fef034f
antuanjoseff <toni.hernandez@udg.edu>
2022-09-26 07:34:32 +0200

    Adding script to build tiles

--- media/loop.sh
+++ media/loop.sh
@@ -10,14 +10,15 @@
 #     done
 # done
 
-for x in {0..63}
+for x in {0..7}
 do
-    for y in {0..63}
+    for y in {0..7}
     do
-    filename=/home/toni/git/mosquito2_backend/media/cache/gadm2/6/$x/$y.pbf
+    filename=/home/toni/git/mosquito2_backend/media/cache/gadm2/3/$x/$y.pbf
     if [ ! -f "$filename" ]; then
-        mkdir /home/toni/git/mosquito2_backend/media/cache/gadm2/6/$x
-    	wget "localhost:8000/api/tiles/gadm2/6/$x/$y" -O /home/toni/git/mosquito2_backend/media/cache/gadm2/6/$x/$y.pbf
+        #mkdir /home/toni/git/mosquito2_backend/media/cache/gadm2/5/$x
+        mkdir E:/toni/sigte/projectes/mosquito2_backend/media/cache/gadm2/3/$x
+    	wget "localhost:8000/api/tiles/gadm2/3/$x/$y" -O E:/toni/sigte/projectes/mosquito2_backend/media/cache/gadm2/3/$x/$y.pbf
     fi
     done
 done
