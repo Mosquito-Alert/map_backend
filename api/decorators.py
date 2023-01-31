7651c714b5c03e8684b4d4abc5a057ac8eba6131
antuanjoseff <toni.hernandez@udg.edu>
2023-01-31 10:15:43 +0100

    New cookie translation + cookie session httponly

--- api/decorators.py
+++ api/decorators.py
@@ -3,29 +3,14 @@ from django.http import HttpResponseForbidden
 import functools
 
 def referrer_cookie_required(view_func):
+    """
+    this decorator ensures that the view func accepts only 
+    session requests
+    """        
     @functools.wraps(view_func)
     def wrapper(request, *args, **kwargs):
-        if request.COOKIES.get('referrer') is None:
+        if request.COOKIES.get('sessionid') is None:
             return HttpResponseForbidden()
         return view_func(request, *args, **kwargs)
 
-    return wrapper
-    
-
-def deny_empty_origin(view_func):
-    """
-    this decorators ensures that the view func accepts only 
-    certain referers
-    """
-    @functools.wraps(view_func)
-    def wrapper(request, *args, **kwargs):
-        origin = request.headers.get('referer')
-        try:
-            for e in allowed_referers:
-                if e in origin:
-                    return view_func(request, *args, **kwargs)
-        except Exception as e:
-            return HttpResponseForbidden()
-
-        return HttpResponseForbidden()
     return wrapper
