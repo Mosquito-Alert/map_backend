e1904b0747cf111170c88470e0d32bd312a65aaa
antuanjoseff <toni.hernandez@udg.edu>
2023-02-07 17:59:59 +0100

    authentification functions with csrf

--- project/urls.py
+++ project/urls.py
@@ -17,12 +17,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
-from .translations import translations, get_csrf_token
+from .translations import translations
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
     path('api/', include('api.urls')),
-    path('csrf/', get_csrf_token),
     path('translations/<lang>/', translations),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
